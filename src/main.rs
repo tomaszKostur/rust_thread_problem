10586ff5c570ac55b427f3567209e484665e406c
Tomasz Kostur <tomasz.kostur@gmail.com>
2020-10-24 09:45:58 +0200

    Problem resolved
    Join method on thread needs ownership so in implemented metgod on
    structure self must me owned like "self" not reference like
    "&mut self" or "&self"

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-mod simpliest {
-    use std::sync::mpsc;
-    use std::thread;
-    use std::time::Duration;
-
-    pub fn foo() {
-        let (tx, rx) = mpsc::channel::<bool>();
-        let closure = 
-            move || {loop {
-                println!("from closure");
-                thread::sleep(Duration::from_secs(1));
-                let should_stop = rx.try_recv();
-                let should_stop = match should_stop {
-                    Ok(_) => true,
-                    Err(_) => false,
-                };
-                if should_stop { break;}
-            }
-        };
-        let j = thread::spawn(closure);
-        thread::sleep(Duration::from_secs(4));
-        tx.send(true).unwrap();
-        j.join().unwrap();
-    }
-}
-
 mod structed {
     use std::sync::mpsc;
     use std::thread;
@@ -39,9 +13,9 @@ mod structed {
     }
 
     impl Interface {
-        pub fn stop_working(&self) {
+        pub fn stop_working(self){
             self.command_pipe.send(InterfaceCommands::StopThread).expect("Unable to stop_working");
-            //self.join_handler.join(); // ? how to do that ?
+            self.join_handler.join().expect("Worker thread cannot be joined somehow")
         }
         pub fn do_something(&self) {
             self.command_pipe.send(InterfaceCommands::DoSomething).expect("Unable to do_something");
@@ -77,9 +51,8 @@ fn main() {
     use std::time::Duration;
 
     let interface = structed::start_worker();
-    thread::sleep(Duration::from_secs(4));
+    thread::sleep(Duration::from_secs(2));
     interface.do_something();
     interface.do_something();
     interface.stop_working();
-    thread::sleep(Duration::from_secs(1)); // sleep oriented destructor ;_;
 }
